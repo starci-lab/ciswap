0b594c84f0458b6d0a4f6968ac613560c7bece69
starci183 <cuongnvtse160875@gmail.com>
2025-07-08 16:25:12 +0700

    123213

--- src/config/aptos.ts
+++ src/config/aptos.ts
@@ -1,9 +1,9 @@
 import { Network } from "@/types"
 
 export const APTOS_SWAP_RESOURCE_TESTNET_ACCOUNT =
-  "0xd0b061dfcd2e4141a3eafde747db8e5639df05eb88d0c4748d2e751be1612e15"
+  "0x6b47a820e52fde2df5a33d454614b5f94702a6c75b0b01e4a94394e01c41b72e"
 export const APTOS_SWAP_RESOURCE_MAINNET_ACCOUNT =
-  "0xd0b061dfcd2e4141a3eafde747db8e5639df05eb88d0c4748d2e751be1612e15"
+  "0x6b47a820e52fde2df5a33d454614b5f94702a6c75b0b01e4a94394e01c41b72e"
 
 export const aptosSwapResourceAccounts = {
     [Network.Mainnet]: APTOS_SWAP_RESOURCE_MAINNET_ACCOUNT,
